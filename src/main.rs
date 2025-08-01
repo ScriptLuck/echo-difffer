1c4fffbd517edd3b2d64e4cf972fe6464e3e512e
ScriptLuck <mikhail.myself@gmail.com>
2025-08-01 12:59:37 +0100

    Fixed latency issue

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,26 @@
 
 // main.rs
 
-// Update 3: Create simple version that works (but with huge latency for some reason)
+// Update 4: Fixed latency issue, now all working just fine
 
+use libpulse_binding::def::BufferAttr;
 use libpulse_binding::sample::{Format, Spec};
 use libpulse_binding::stream::Direction;
 use libpulse_simple_binding::Simple;
 
 fn main() {
     let spec = Spec {
-        format: Format::S32le, // or whatever format you want
+        format: Format::S32le,
         channels: 2,
         rate: 44100,
+    };
+
+    let buffer_attr = BufferAttr {
+        maxlength: u32::MAX, // Maximum buffer length ~ auto
+        tlength: 1024,       // Target length (samples)
+        prebuf: 1024,        // Pre-buffering
+        minreq: 256,         // Minimum request
+        fragsize: 256,       // Fragment size
     };
 
     let record = Simple::new(
@@ -23,7 +32,7 @@ fn main() {
         "Audio Record",
         &spec,
         None,
-        None,
+        Some(&buffer_attr),
     )
     .expect("Failed to create simple PulseAudio connection");
 
@@ -31,7 +40,7 @@ fn main() {
         None,
         "Echo Difffer",
         Direction::Playback,
-        Some("alsa_output.pci-0000_00_1f.3.analog-stereo"), // Your output
+        Some("alsa_output.pci-0000_00_1f.3.analog-stereo"), // Output device
         "Audio Playback",
         &spec,
         None,
